6d58d3841d23899a01991f8fcda142d5caa6954a
Miguel <81305587+MiguelCF94@users.noreply.github.com>
2022-02-11 14:54:29 +0100

    Add files via upload

---
+++ src/ahorcado/ahorcado.py
@@ -0,0 +1,151 @@
+MUNECO = ['''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========''']
+
+
+def bienvenida():
+    print('*' * 37)
+    print('* Bienvenido al juego del ahorcado *')
+    print('*' * 37)
+
+
+
+def inicializar_juego():
+    palabra = input('Introduzca la palabra a adivinar ').lower()
+    tablero = ['_'] * len(palabra)
+    return tablero, palabra, []
+
+
+
+def mostrar_escenario(errores):
+    print(MUNECO[errores])
+
+
+
+def mostrar_tablero(tablero, letras_erroneas):
+    for casilla in tablero:
+        print(casilla, end=' ')
+    print()
+    print()
+    if len(letras_erroneas) > 0:
+        print('Letras err�neas:', *letras_erroneas)
+        print()
+
+
+
+def pedir_letra(tablero, letras_erroneas):
+    valida = False
+    while not valida:
+        letra = input('Introduce una letra: ').lower()
+        valida = 'a' <= letra <= 'z' and len(letra) == 1 # es una letra
+        if not valida:
+            print('Error, no puede ser un s�mbolo ni un n�mero.')
+        else:
+            valida = letra not in tablero + letras_erroneas
+            if not valida:
+                print('Letra repetida, �Por qu� no intenta otra nueva?')
+
+    return letra
+
+
+def procesar_letra(letra, palabra, tablero, letras_erroneas):
+    if letra in palabra:
+        print('Has acertado una letra.')
+        actualizar_tablero(letra, palabra, tablero)
+    else:
+        print('Has fallado.')
+        letras_erroneas.append(letra)
+
+
+def actualizar_tablero(letra, palabra, tablero):
+    for indice, letra_palabra in enumerate(palabra):
+        if letra == letra_palabra:
+            tablero[indice] = letra
+
+
+def comprobar_palabra(tablero):
+    return '_' not in tablero
+
+
+# bucle principal de juego
+def jugar_al_ahorcado():
+    try:
+        tablero, palabra, letras_erroneas = inicializar_juego()  # paso 1
+        while len(letras_erroneas) < len(MUNECO):  # pasos 7 y 8
+            mostrar_escenario(len(letras_erroneas))  # paso 2
+            mostrar_tablero(tablero, letras_erroneas)  # paso 3
+            if len(letras_erroneas)<len(MUNECO):
+                letra = pedir_letra(tablero, letras_erroneas)  # paso 4
+            procesar_letra(letra, palabra, tablero, letras_erroneas)  # paso 5
+            if comprobar_palabra(tablero):  # paso 6
+                print('�Enhorabuena, lo has logrado!')
+                break
+            else:
+                print(f'�Lo siento! �Has perdido! La palabra a adivinar era {palabra}.')
+                mostrar_escenario(len(letras_erroneas))  # paso 7
+    
+        mostrar_tablero(tablero, letras_erroneas)
+    except IndexError:
+        print()    
+
+
+def jugar_otra_vez():
+    return input('Deseas jugar otra vez (introduce s para s� o cualquier otra cosa para no): ')
+
+
+
+
+if __name__ == '__main__':
+
+
+    bienvenida()
+    while True:
+        jugar_al_ahorcado()
+        if jugar_otra_vez() != 's': break
---
+++ src/mastermind/mastermind.py
@@ -0,0 +1,61 @@
+import random
+
+def main(): 
+    comprueba(generar())
+    
+def generar():
+    numString=list()
+    lista=list()
+    
+    for i in range(0,4):
+        aleatorio=random.randint(0, 9)
+        while aleatorio in lista:
+            aleatorio=random.randint(0, 9)
+        lista.append(aleatorio)    
+    
+    for i in range(0,len(lista),1):
+        numString+=str(lista[i])    
+        
+    print(numString)
+    return numString   
+
+
+def comprueba(num):
+    listaEntrada=list()
+    listaMuertos=list()
+    listaHeridos=list()
+        
+    numEntrada=(input("Introduce numero "))
+    
+    while True:
+        while len(numEntrada)!=4:
+            numEntrada=(input("Introduce numero "))
+            
+        for i in range(0,4):
+            listaEntrada.append(numEntrada[i])
+        
+        if num==listaEntrada:
+            print("Acertado")
+            return 
+            
+        else:
+            for i in range(0,4):
+                if num[i]==listaEntrada[i] and listaEntrada[i] not in listaMuertos:
+                    listaMuertos.append(listaEntrada[i])
+                elif listaEntrada[i] in num and listaEntrada[i] not in listaHeridos:
+                    listaHeridos.append(listaEntrada[i])
+            
+            print("Muertos:")
+            for i in range(len(listaMuertos)):
+                print(listaMuertos[i])
+            
+            print("Heridos:")
+            for i in range(len(listaHeridos)):   
+                print(listaHeridos[i])
+                
+        listaEntrada.clear()  
+        numEntrada=(input("Introduce numero "))
+    
+
+if __name__ == '__main__':
+    main()
